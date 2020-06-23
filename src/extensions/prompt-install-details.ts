5dcf9f42e5722f5d62cfc0c5d646237ed06ebc90
bankkasem <kasemsarn@agentone.co.th>
2020-06-23 10:57:58 +0700

    update promp install details

---
+++ src/extensions/prompt-install-details.ts
@@ -0,0 +1,86 @@
+const path = require('path')
+
+import { prompt, GluegunToolbox } from 'gluegun'
+import { IInstallDetails } from '../types'
+
+module.exports = async (toolbox: GluegunToolbox) => {
+	toolbox.promptInstallDetails = async (firstParam?: string) => {
+		const { filesystem } = toolbox
+
+		const defaultAppName = firstParam || path.basename(filesystem.path())
+
+		const askAppName = {
+			type: 'input',
+			name: 'appName',
+			message: 'Project name',
+			default: defaultAppName
+		}
+
+		const askAppVersion = {
+			type: 'input',
+			name: 'appVersion',
+			message: 'Version',
+			default: '1.0.0'
+		}
+
+		const askAppDescription = {
+			type: 'input',
+			name: 'appDescription',
+			message: 'Description'
+		}
+
+		const askAppAuthor = {
+			type: 'input',
+			name: 'appAuthor',
+			message: 'Author',
+			default: 'agent one co., ltd.'
+		}
+
+		const askVueModules = {
+			type: 'multiselect',
+			name: 'vueModules',
+			message:
+				'Choose Vue Modules (Press <space> to select, <a> to toggle all, <i> to invert selection)',
+			choices: ['vue-meta', 'vue-router']
+		}
+
+		const askWantHttps = {
+			type: 'confirm',
+			name: 'wantHttps',
+			message: 'Want HTTPS?'
+		}
+
+		const questions = [
+			askAppName,
+			askAppVersion,
+			askAppDescription,
+			askAppAuthor,
+			askVueModules,
+			askWantHttps
+		]
+
+		const details: IInstallDetails = await prompt.ask(questions)
+
+		if (details.wantHttps) {
+			const askCertPath = {
+				type: 'input',
+				name: 'httpsCertPath',
+				message: 'Cert Path',
+				default: '.'
+			}
+
+			const askKeyPath = {
+				type: 'input',
+				name: 'httpsKeyPath',
+				message: 'Key Path',
+				default: '.'
+			}
+
+			const httpsQuestions = [askCertPath, askKeyPath]
+
+			await prompt.ask(httpsQuestions)
+		}
+
+		return details
+	}
+}
