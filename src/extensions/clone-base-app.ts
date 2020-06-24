80329b5d95bb41fb3b09235795a23235699938ed
bankkasem <kasemsarn@agentone.co.th>
2020-06-24 13:48:17 +0700

    update extensions/clone-base-app

--- src/extensions/clone-base-app.ts
+++ src/extensions/clone-base-app.ts
@@ -4,7 +4,7 @@ module.exports = async (toolbox: GluegunToolbox) => {
 	toolbox.cloneBaseApp = async (baseAppUrl: string) => {
 		const { system } = toolbox
 
-		await system.spawn(`git clone ${baseAppUrl} base_app`, {
+		await system.spawn(`git clone ${baseAppUrl} mk-base`, {
 			shell: true,
 			stdio: 'inherit',
 			stderr: 'inherit'
